44036c2b8a65ef05b7c90f58fbe532ae5c210330
ahmad rafiansyah <ahmad rafiansyab20@gmail.com>
2025-11-24 21:13:25 +0700

    update personalisasi page

--- lib/screens/personalisasi_page.dart
+++ lib/screens/personalisasi_page.dart
@@ -1,1 +1,21 @@
-// haloo
+import 'package:flutter/material.dart';
+
+class PersonalisasiPage extends StatelessWidget {
+  const PersonalisasiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Personalisasi Page')),
+      body: const Center(
+        child: Text(
+          'Welcome to the Personalisasi Page!',
+          style: TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
